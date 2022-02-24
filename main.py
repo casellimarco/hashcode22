2fd069703fa6a22dc2185a16081909d7ba35c42f
Marco Caselli <marco.caselli@mindfoundry.ai>
2022-02-24 20:09:55 +0000

    That's all I got

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from base_class import score
+from score_module import get_score
 from io_manager import read_input, write_output
-from strategy import method
+from strategy import method_3 as method
 
 
-for char in "abcde":
-    cls = read_input(char)
-    result = method(cls)
-    score = get_score(cls, result)
-    print(f"Case {char}: {score} score \n")
+for char in "abcdef":
+    contributors, projects = read_input(char)
+    print(f"Num contributors: {len(contributors)}")
+    print(f"Num projects: {len(projects)}")
+
+    result = method(contributors, projects)
+    # score = get_score(result, projects, contributors)
+    # print(f"Case {char}: {score} score \n")
+    print(char)
+
     write_output(result, char)
